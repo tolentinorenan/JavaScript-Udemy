dfdde741add7e4bd62c8211e9e74d5eecea676da
Renan de Almeida <89488800+tolentinorenan@users.noreply.github.com>
2022-04-14 14:37:11 -0300

    estrutura

--- Professor Leonardo Moura/estrutura.js
+++ Professor Leonardo Moura/estrutura.js
@@ -1,1 +1,18 @@
-console.log('oka')
+function soma(valor1, valor2){
+    console.log(valor1 + valor2);
+}
+//soma(1,2);
+function somaReturn(valor1, valor2){
+    let resultado = valor1 + valor2;
+    return resultado;
+}
+//console.log(somaReturn(2,5));
+//let umafuncao = soma;
+//umafuncao(1,2222);
+let novaFunc = function(teste){
+    console.log("Olá");
+    console.log(teste);
+}
+novaFunc("teste teste teste");
+novaFunc(123);
+novaFunc(1 == 1);
