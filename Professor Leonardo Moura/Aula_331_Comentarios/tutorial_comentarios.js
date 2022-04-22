3ad1af81e37f205399fb711a57ba82f34681da37
Renan de Almeida <89488800+tolentinorenan@users.noreply.github.com>
2022-04-22 12:02:48 -0300

    main

---
+++ Professor Leonardo Moura/Aula_331_Comentarios/tutorial_comentarios.js
@@ -0,0 +1,20 @@
+// Comentario de uma linha
+
+console.log('LInha 1') // PODE SER AQUI TB
+
+/*
+Comentario de multiplas
+linhas
+*/
+
+console.log('Linha 2')
+
+/*
+*Comentário de
+multiplas linhas
+*
+*
+*/
+
+console.log('linah 3')
+
