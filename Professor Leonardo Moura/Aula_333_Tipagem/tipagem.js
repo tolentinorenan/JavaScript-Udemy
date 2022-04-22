21e00fe533ae541d207abaec7d5452cacd33bd25
Renan de Almeida <89488800+tolentinorenan@users.noreply.github.com>
2022-04-22 12:54:17 -0300

    tipagem dinamica

---
+++ Professor Leonardo Moura/Aula_333_Tipagem/tipagem.js
@@ -0,0 +1,12 @@
+// JS É UMA LINGUAGEM DE TIPAGEM DINAMICA (fraca)
+
+let qualquer = 'legal'
+console.log(typeof qualquer)  // MOSTRA A TIPAGEM DO LET
+
+qualquer = 3.1516
+console.log(typeof qualquer)
+
+// Evitar nome genpericos e siglas
+
+let valor = ''
+let numero = 1
